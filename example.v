71180a0cb0ae55a37450bcd08d02e18aa403b57e
clifford <clifford@2e668e65-2634-0410-9be0-b11ca6b5c640>
2015-10-24 14:51:43 +0000

    ...

--- example.v
+++ example.v
@@ -1,7 +1,8 @@
 module top (input clk, output reg led1, led2, led3);
-	reg [23:0] counter = 0;
+	parameter COUNTER_BITS = 25;
+	reg [COUNTER_BITS-1:0] counter = 0;
 	always @(posedge clk) begin
 		counter <= counter + 1;
-		{led1, led2, led3} <= counter[23:21];
+		{led1, led2, led3} <= counter[COUNTER_BITS-1 -: 3] ^ (counter[COUNTER_BITS-1 -: 3] << 1);
 	end
 endmodule
